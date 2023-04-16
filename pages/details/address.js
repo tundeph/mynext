ed857db856f16463fa952f89ec50914b6731266d
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-16 22:14:39 +0100

    Create address.js

---
+++ pages/details/address.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Navbar from "../../components/navbar"
+
+const Address = () => {
+	return (
+		<div>
+			<Navbar />
+			Address
+		</div>
+	)
+}
+
+export default Address
