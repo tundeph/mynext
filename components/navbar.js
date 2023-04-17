b9700e9cff0e53025c8292c49a8a64cb00b3f096
tundeph <54335828+tundeph@users.noreply.github.com>
2023-04-18 00:06:13 +0100

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -3,13 +3,14 @@ import Link from "next/link"
 
 const Navbar = () => {
 	return (
-		<div>
-			<nav style={{ display: "flex", gap: 12 }}>
-				<Link href="/">Home</Link>
-				<Link href="/about">About</Link>
-				<Link href="/details/address">Address</Link>
-			</nav>
-		</div>
+		<nav style={{ display: "flex", gap: 12 }}>
+			<div className="logo">
+				<h3> Logo</h3>
+			</div>
+			<Link href="/">Home</Link>
+			<Link href="/about">About</Link>
+			<Link href="/details/address">Address</Link>
+		</nav>
 	)
 }
 
